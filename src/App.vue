4e99ac0efcc3879ae78fd6d05a686912e54487e9
jgrossrieder <julien.grossrieder@ch.meggitt.com>
2022-11-13 14:22:36 +0100

    Creating minimum reproduction for vue issue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,141 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="h-full min-h-screen flex justify-content-center align-items-center">
+    <Card class="w-10 max-w-lg">
+      <template #content>
+        <div class="text-900 text-3xl font-medium mb-3 text-center">
+          Please authenticate
+        </div>
+        <TabView class="centered">
+          <TabPanel header="Login">
+            <form class="flex flex-column">
+              <div class="field h-full">
+                <label>Email</label>
+                <span class="p-input-icon-left w-full">
+                  <i class="fa-regular fa-at"></i>
+                  <InputText class="w-full" type="text" />
+                </span>
+                <!-- <app-field-errors
+                  fieldName="Email"
+                ></app-field-errors> -->
+              </div>
+              <div class="field h-full">
+                <label>Password</label>
+                <span class="p-input-icon-left w-full">
+                  <i class="fa-regular fa-key"></i>
+                  <InputText
+                    class="w-full"
+                    type="password"
+                    inputClass="w-full"
+                    :feedback="false"
+                  />
+                </span>
+                <!-- <app-field-errors
+                  [formField]="loginForm.controls['password']"
+                  fieldName="Password"
+                ></app-field-errors> -->
+              </div>
+              <div>
+                <!-- <app-field-error
+                  *ngIf="invalidLoginCredentials"
+                  class="text-base flex justify-content-center mb-3"
+                >
+                  Invalid credentials
+                </app-field-error> -->
+              </div>
+              <Button
+                type="submit"
+                label="Sign In"
+                icon="fa-regular fa-user-unlock"
+              ></Button>
+              <Divider align="center" class="m-3">
+                <span>or sign in with...</span>
+              </Divider>
+              <div class="flex justify-content-evenly">
+                <Button
+                  type="button"
+                  style="background-color: #dd4b39"
+                  icon="fa-brands fa-google"
+                ></Button>
+                <Button
+                  type="button"
+                  style="background-color: #1877f2"
+                  icon="fa-brands fa-facebook"
+                ></Button>
+                <Button
+                  type="button"
+                  style="background-color: #333"
+                  icon="fa-brands fa-github"
+                ></Button>
+              </div>
+            </form>
+          </TabPanel>
+          <TabPanel header="Register">
+            <form class="flex flex-column">
+              <div class="field h-full">
+                <label>Email</label>
+                <span class="p-input-icon-left w-full">
+                  <i class="fa-regular fa-at"></i>
+                  <InputText class="w-full" type="text" />
+                </span>
+                <!-- <app-field-errors
+                  [formField]="registerForm.controls['email']"
+                  fieldName="Email"
+                ></app-field-errors> -->
+              </div>
+              <div class="field h-full">
+                <label>Password</label>
+                <span class="p-input-icon-left w-full">
+                  <i class="fa-regular fa-key"></i>
+                  <Password class="w-full" type="password" autocomplete="new-password" />
+                </span>
+                <!-- <app-field-errors
+                  [formField]="registerForm.controls['password']"
+                  fieldName="Password"
+                ></app-field-errors> -->
+              </div>
+              <div>
+                <!-- <app-field-error
+                  *ngIf="invalidRegisterCredentials"
+                  class="text-base flex justify-content-center mb-3"
+                >
+                  Invalid credentials
+                </app-field-error> -->
+              </div>
+              <Button
+                type="submit"
+                label="Register"
+                icon="fa-regular fa-user-unlock"
+              ></Button>
+              <Divider align="center" class="m-3">
+                <span>or register in with...</span>
+              </Divider>
+              <div class="flex justify-content-evenly">
+                <Button
+                  type="button"
+                  style="background-color: #dd4b39"
+                  icon="fa-brands fa-google"
+                ></Button>
+                <Button
+                  type="button"
+                  style="background-color: #1877f2"
+                  icon="fa-brands fa-facebook"
+                ></Button>
+                <Button
+                  type="button"
+                  style="background-color: #333"
+                  icon="fa-brands fa-github"
+                ></Button>
+              </div>
+            </form>
+          </TabPanel>
+        </TabView>
+      </template>
+    </Card>
+  </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+div.p-tabview.centered .p-tabview-nav {
+  justify-content: center;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
 import App from './App.vue';
 import router from './router';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .use(PrimeVue)
+  .component('TabView', TabView)
+  .component('TabPanel', TabPanel)
+  .component('Card', Card)
+  .component('InputText', InputText)
+  .component('Button', Button)
+  .component('Divider', Divider)
+  .mount('#app');
